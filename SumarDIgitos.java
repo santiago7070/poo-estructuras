1724c2146c040d28f1bd05be70c3f42f0e30b57c
santiago7070 <bohorquezsantiago771@gmail.com>
2026-05-22 11:46:00 -0500

    prueba

---
+++ SumarDIgitos.java
@@ -0,0 +1,15 @@
+public class SumarDIgitos {
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in)
+        
+        
+        int SumarDIgitos (int n) {
+                String numero = n + "";
+                  int suma = 0;
+                  for(int i = 0; i < nuumero.length(); i++) {
+                   suma = suma + (numero.CharAt(i)- "0");
+
+                  }
+            }
+    }
+}
